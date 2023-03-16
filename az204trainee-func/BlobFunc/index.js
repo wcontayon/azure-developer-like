ab403f8752356361b9bbf268f89ed125706a1bf2
wcontayon <williamcontayon@outlook.com>
2023-03-16 14:53:07 +0000

    update branch

--- az204trainee-func/BlobFunc/index.js
+++ az204trainee-func/BlobFunc/index.js
@@ -1,5 +1,18 @@
+const { CosmosClient } = require("@azure/cosmos")
+
 module.exports = async function (context, myBlob) {
     context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
+    console.log(myBlob);
+    
+    const endpoint = "https://satrainee12.documents.azure.com:443/";
+    const key = "";
+    const client = new CosmosClient({ endpoint, key });
+
+    const { database } = await client.databases.createIfNotExists({id: "UserFeedback"});
+    const { container } = await database.containers.createIfNotExists({id: "user_feedback"})
+
+    console.log(container);
+    
     // To Do : Do your traitements
     context.bindings.myOutputBlob = myBlob;
 };
